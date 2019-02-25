602d79e0a5261f5afce957d5e88d96000cd657dd
Ryan Quinn <fowardingtoyou@gmail.com>
2019-02-24 19:52:31 -0500

    Update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :aonyx, AonyxWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "107.173.67.49", port: 4001],
+  check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
